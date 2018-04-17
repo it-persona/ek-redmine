04ef946109729a488c8c11a5aaf8337ff01e84d1
Vadym Panchenko <panchenko.vadym@gmail.com>
2018-04-17 15:04:30 +0300

    security

---
+++ src/AppBundle/Controller/Redmine/SecurityController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Controller\Redmine;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Template(":security:login.html.twig")
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ];
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+        return $this->redirect($this->generateUrl('logout'));
+    }
+
+}
